be0fb295ad7c878321888e93b9c3597618cc76f5
Kevin Lobsinger <kevlobsinger@gmail.com>
2023-04-18 18:49:24 -0400

    iterating Over Arrays

---
+++ packing-list.js
@@ -0,0 +1,7 @@
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+console.log('kitchen stuff to pack:');
+for(i = 0; i < packingList.length; i++){
+
+  console.log(packingList[i])
+}
